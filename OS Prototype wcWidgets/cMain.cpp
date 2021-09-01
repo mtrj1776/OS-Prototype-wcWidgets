8c274b8da78bc7e039d6391f6708e6c0adf1b9b6
Michael Johns <mtrj1776@gmail.com>
2021-09-01 17:14:23 +0100

    Updated button

--- OS Prototype wcWidgets/cMain.cpp
+++ OS Prototype wcWidgets/cMain.cpp
@@ -9,37 +9,56 @@ Author: Michael Johns
 // Event Table Entries macro
 // (class event is for, base class)
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
-	// Button Pressed (Windows button ID, Function to call)
-	EVT_BUTTON(10001, OnButtonClicked)
+// Button Pressed (Windows button ID, Function to call)
+EVT_BUTTON(10001, OnButtonClicked)
 wxEND_EVENT_TABLE()
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "OS Prototype wcWidgets", wxDefaultPosition, wxDefaultSize)
 {
 	// Construct button (parent, Frame ID, Button Text, Position inside parent frame, button size
-	m_btn1 = new wxButton(this, 10001, "Click ME", wxPoint(10, 10), wxSize(150, 50));
-	m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
-	m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
+
+	//m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
+	//m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
+
+	// App Background Container Panel
+	m_panelBackground = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+	m_panelBackground->SetBackgroundColour(wxColour(100, 200, 200));
+
 
 	// Layout Panel background and colour
-	m_panelLayout = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
+	m_panelLayout = new wxPanel(m_panelBackground, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
 	m_panelLayout->SetBackgroundColour(wxColour(100, 100, 200));
 
 	// Data Panel
-	m_panelData = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(300, 50));
-	m_panelData->SetBackgroundColour(wxColor(100, 200, 100));
+	m_panelData = new wxPanel(m_panelLayout, wxID_ANY, wxDefaultPosition, wxSize(40, 10));
+	m_panelData->SetBackgroundColour(wxColor(200, 200, 100));
+
+	// Test Panel
+	m_panelData = new wxPanel(m_panelBackground, wxID_ANY, wxDefaultPosition, wxSize(300, 50));
+
+	// Add panel sizer to control size
+	m_sizer2 = new wxBoxSizer(wxVERTICAL);
+	m_sizer2->Add(m_panelBackground, 1, wxEXPAND | wxALL);
+	
 
 	// Add panel sizer to control size
 	m_sizer1 = new wxBoxSizer(wxVERTICAL);
+	//m_sizer1->Add(m_panelBackground, 1, wxEXPAND);
 
 	// Add Sizer(panel the sizer is controlling, amount of area to expand to, expand type | margins)
 
 	// Layout Panel
 	m_sizer1->Add(m_panelLayout, 1, wxEXPAND | wxLEFT | wxTOP | wxRIGHT, 5);
 
+	//m_btn1 = new wxButton(m_panelLayout, 10001, "Click ME", wxPoint(10, 10), wxSize(150, 50));
+	m_btn1 = new wxButton(m_panelLayout, 10001, "Layout", wxPoint(10, 10), wxSize(150, 50), wxBORDER_NONE | wxEXPAND );
+	m_btn1->SetBackgroundColour(wxColor(100, 100, 100));
+
 	// Data Panel
 	m_sizer1->Add(m_panelData, 1, wxEXPAND | wxALL, 5);
 
 	this->SetSizerAndFit(m_sizer1);
+	this->SetSizerAndFit(m_sizer2);
 }
 
 cMain::~cMain()
@@ -49,8 +68,12 @@ cMain::~cMain()
 
 void cMain::OnButtonClicked(wxCommandEvent& evt)
 {
+	m_sizer1->Hide(m_panelLayout);
+	m_sizer1->Layout();//refresh panel sizes
+
+
 	// append string
-	m_list1->AppendString(m_txt1->GetValue());
+	//m_list1->AppendString(m_txt1->GetValue());
 	// Tell event has finished
 	evt.Skip();
 }
--- OS Prototype wcWidgets/cMain.h
+++ OS Prototype wcWidgets/cMain.h
@@ -14,8 +14,11 @@ public:
 	wxTextCtrl* m_txt1 = nullptr;
 	wxListBox* m_list1 = nullptr;
 
+	wxPanel* m_panelBackground = nullptr;
 	wxPanel* m_panelLayout = nullptr;
 	wxPanel* m_panelData = nullptr;
+	wxPanel* m_panelTest = nullptr;
+	wxBoxSizer* m_sizer2 = nullptr;
 	wxBoxSizer* m_sizer1 = nullptr;
 
 	// Class Functions
